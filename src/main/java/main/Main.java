52d215d01ff0df45535509b9338bf4a99e048f47
Marek Tałaj <marekt1707@gmail.com>
2021-05-10 20:11:52 +0200

    project initialization

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String block = """
+                Hello
+World
+      from
+          blocks
+
+                """;
+        System.out.println(block);
+    }
+}
